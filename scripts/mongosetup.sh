8297ae07d2d80a725cbe6c2f332d32ce51442ce7
Craig Duncan <craig@CraigMBPR.local>
2017-01-10 10:51:17 +0000

    Updated to include 5 nodes

--- scripts/mongosetup.sh
+++ scripts/mongosetup.sh
@@ -3,20 +3,22 @@
 MONGODB1=`ping -c 1 mongo1 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 MONGODB2=`ping -c 1 mongo2 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 MONGODB3=`ping -c 1 mongo3 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+MONGODB4=`ping -c 1 mongo4 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+MONGODB5=`ping -c 1 mongo5 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 
 echo "Waiting for startup.."
-until curl http://${MONGODB1}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
+until curl http://${MONGODB1}:27017/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
   printf '.'
   sleep 1
 done
 
-echo curl http://${MONGODB1}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1
+echo curl http://${MONGODB1}:27017/serverStatus\?text\=1 2>&1 | grep uptime | head -1
 echo "Started.."
 
 
-echo SETUP.sh time now: `date +"%T" `
+echo mongosetup.sh time now: `date +"%T" `
 mongo --host ${MONGODB1}:27017 <<EOF
-   var cfg = {
+   var config = {
         "_id": "my-mongo-set",
         "members": [
             {
@@ -30,9 +32,17 @@ mongo --host ${MONGODB1}:27017 <<EOF
             {
                 "_id": 2,
                 "host": "${MONGODB3}:27017"
+            },
+            {
+                "_id": 3,
+                "host": "${MONGODB4}:27017"
+            },
+            {
+                "_id": 4,
+                "host": "${MONGODB5}:27017"
             }
         ]
     };
-    rs.initiate(cfg, { force: true });
-    rs.reconfig(cfg, { force: true });
+    rs.initiate(config, { force: true });
+    rs.reconfig(config, { force: true });
 EOF
